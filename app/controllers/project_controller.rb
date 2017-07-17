a0370166587ffe55fe28b5d8ac327b798a044e97
yoshi522 <yoshihito522@gmail.com>
2017-07-17 15:46:58 +0900

    Added sample.

---
+++ app/controllers/project_controller.rb
@@ -0,0 +1,7 @@
+class ProjectController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'project/index'
+
+  get 'project/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+
+  root 'project#index'
 end
