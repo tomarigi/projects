0811c986100e00dcc55721dee1f153e5b22d3dc0
yoshi522 <yoshihito522@gmail.com>
2017-08-05 12:35:07 +0900

    Added search logic.

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,7 +1,22 @@
 class ProjectController < ApplicationController
+  require "json"
   def index
     json = ActiveSupport::JSON.decode(File.read('projects.json'))
     @projects = json.sort { |x, y| Time.parse(x["updated"]) <=> Time.parse(y["updated"]) }.reverse
+
+    query = params[:q]
+    if query != ""
+      results = []
+
+      # Search in json with query
+      @projects.each do |obj|
+        result = obj.select {|k, v| v.include?(query)}
+        results.push(obj) unless result.empty?
+      end
+
+      @projects = results
+    end
+
   end
 
   def show
