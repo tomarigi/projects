b2d3e48666dde8f71b7368d4c7bed6d2a6d1d063
yoshi522 <yoshihito522@gmail.com>
2017-08-05 12:44:56 +0900

    Remove devise and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,4 @@ Rails.application.routes.draw do
   resources :project, only: [:index, :show]
 
   root 'project#index'
-
-  devise_for :users, :controllers => {
-      :omniauth_callbacks => "omniauth_callbacks"
-  }
 end
