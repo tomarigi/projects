522c4afa73b161a20dce68c46e733cd3d8f463b5
yoshi522 <yoshihito522@gmail.com>
2017-08-15 21:26:41 +0900

    Redirect to edit page unless user visit more than one time.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   layout 'user/application'
-  def hello
-    render html: "hello"
+  # saves the location before loading each page so we can return to the
+  # right page. If we're on a devise page, we don't want to store that as the
+  # place to return to (for example, we don't want to return to the sign in page
+  # after signing in), which is what the :unless prevents
+  before_action :store_current_location, :unless => :devise_controller?
+
+  # https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+  private
+  # override the devise helper to store the current location so we can
+  # redirect to it after loggin in or out. This override makes signing in
+  # and signing up work automatically.
+  def store_current_location
+    store_location_for(:user, request.url)
   end
+
+  def after_sign_in_path_for(resource)
+    # redirect user edit path if user visit first time, to change password.
+    current_user.sign_in_count < 2 ? edit_user_registration_path : root_path
+  end
+
 end
