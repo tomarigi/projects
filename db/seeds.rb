64512d988d2e35206f6250c2802fad6571b28d94
yoshi522 <yoshihito522@gmail.com>
2017-08-23 22:39:09 +0900

    [develop.user_profile] seeds: Modified seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+user = User.create!(email:'yoshihito522@gmail.com', password: '123123', password_confirmation: '123123')
+user.create_profile(name: '高柴 慶人', term: '1')
