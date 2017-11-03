a76e265f5149266e437db75cddfc398b4b59ef33
yoshi522 <yoshihito522@gmail.com>
2017-11-03 09:16:01 +0900

    [master] fixed default url option

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { host: 'projects.tobitate-tomarigi.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'projects.tobitate-tomarigi.org' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
@@ -73,7 +73,7 @@ Rails.application.configure do
       :authentication => :plain,
       :user_name      => ENV['SENDGRID_USERNAME'],
       :password       => ENV['SENDGRID_PASSWORD'],
-      :domain         => 'projects.tobitate-tomarigi.org',
+      :domain         => 'tobitate-tomarigi.org',
       :enable_starttls_auto => false
   }
 
