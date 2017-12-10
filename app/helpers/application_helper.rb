9ab561519a714e75e7b5d6c36ebfc0a0df08f791
yoshi522 <yoshihito522@gmail.com>
2017-12-10 20:38:21 +0900

    [enhance.#52] add facebook open graph

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,23 @@ module ApplicationHelper
       redirect_to(root_url)
     end
   end
+
+  def full_title(page_title)
+    base_title = 'Tomarigi Portfolio'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+
+  def full_description(description)
+    base_description = 'トビタテ生が主体的に関わるプロジェクトの可視化を行なっています'
+    if description.empty?
+      base_description
+    else
+      "#{description} | #{base_description}"
+    end
+  end
+
 end
